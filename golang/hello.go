721a6722685cd96feb59b943cf84e8c4308970da
Jared Key <key.jared.a@gmail.com>
2022-12-18 17:39:11 +0100

    days 7 part 1 - golang

---
+++ golang/hello.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// fmt.Println(quote.Glass())
+	dat, err := os.ReadFile("../inputs/files.txt")
+	check(err)
+	file_string := strings.Split(string(dat), "\n")
+	root := create_file_tree(file_string)
+	// for _, child := range root.children {
+	// 	if child.is_file {
+	// 		fmt.Println(child.name)
+	// 	}
+	// }
+	calculate_directory_size(root)
+	size := sum_directory_size(root)
+	fmt.Println(size)
+	total_space := 70000000
+	used_space := root.size
+	needed_unused_space := 30000000
+	minimum_deletion_size := used_space - (total_space - needed_unused_space)
+	fmt.Println(minimum_deletion_size)
+}
+
+type Node struct {
+	name     string
+	is_file  bool
+	size     int
+	children []*Node
+	parent   *Node
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func create_file_tree(file_string []string) *Node {
+	root := Node{
+		name:     "/",
+		is_file:  false,
+		size:     0,
+		children: []*Node{},
+		parent:   nil,
+	}
+	active_dir := &root
+	for _, s := range file_string {
+		if s != "" {
+			fields := strings.Fields(s)
+			if string(s[0]) == "$" {
+				// command
+				if len(fields) == 3 {
+					// change dir command
+					active_dir = change_dir(&root, active_dir, fields[2])
+					// fmt.Println(active_dir.name)
+				} else {
+					// ls command
+				}
+			} else if string(s[0:3]) == "dir" {
+				// directory
+				name := fields[1]
+				create_child(active_dir, name, 0, false)
+			} else {
+				// file
+				size, _ := strconv.Atoi(fields[0])
+				name := fields[1]
+				create_child(active_dir, name, size, true)
+			}
+		}
+	}
+	return &root
+}
+
+func change_dir(root *Node, active_dir *Node, next_dir string) *Node {
+	if next_dir == "/" {
+		// fmt.Println("Change to root")
+		return root
+	} else if next_dir == ".." {
+		if active_dir.name != "/" {
+			return active_dir.parent
+		} else {
+			return root
+		}
+	} else {
+		for _, child := range active_dir.children {
+			if next_dir == child.name {
+				return child
+			}
+		}
+		fmt.Println("This child does not exist!")
+	}
+	return root
+}
+
+func create_child(active_dir *Node, name string, size int, is_file bool) {
+	exists := false
+
+	for _, child := range active_dir.children {
+		if name == child.name {
+			exists = true
+		}
+	}
+	if !exists {
+		active_dir.children = append(active_dir.children, &Node{
+			name:     name,
+			is_file:  is_file,
+			size:     size,
+			children: []*Node{},
+			parent:   active_dir,
+		})
+	}
+}
+
+func calculate_directory_size(root *Node) int {
+	for _, child := range root.children {
+		if child.is_file {
+			root.size += child.size
+		} else {
+			root.size += calculate_directory_size(child)
+		}
+	}
+	return root.size
+}
+
+func sum_directory_size(root *Node) int {
+	size := 0
+	for _, child := range root.children {
+		if !child.is_file {
+			size += sum_directory_size(child)
+		}
+	}
+	if root.size <= 100000 {
+		size += root.size
+	}
+	return size
+}
+
+// func get_deletion_directory_size(root *Node) int {
+
+// }
--- src/main.rs
+++ src/main.rs
@@ -22,61 +22,61 @@ fn main() {
 pub fn file_tree_print() {
     let contents = import_content::import("inputs/files.txt");
     let split = contents.split('\n');
-    let f = File::new("dav", 20);
-    let mut d = Directory::new("c");
-    println!("{}", f.size);
-    d.directories.push(Directory::new("y"));
-    d.files.push(f);
-    for q in d.files{
-        println!("{}", q.name);
-    }
-    for sd in d.directories{
-        println!("{}", sd.name);
-    }
-    for s in split{
-        if s != "" {
-            println!("{}", s);
-            break
-        }
-    }
+    // let f = File::new("dav", 20);
+    // let mut d = Directory::new("c");
+    // println!("{}", f.size);
+    // d.directories.push(Directory::new("y"));
+    // d.files.push(f);
+    // for q in d.files{
+    //     println!("{}", q.name);
+    // }
+    // for sd in d.directories{
+    //     println!("{}", sd.name);
+    // }
+    // for s in split{
+    //     if s != "" {
+    //         println!("{}", s);
+    //         break
+    //     }
+    // }
 }
 
-struct File {
-    size: usize,
-    name: String
-}
+// struct File {
+//     size: usize,
+//     name: String
+// }
 
-impl File {
-    pub fn new(name: &str, size: usize) -> File {
-        File {
-            size: size,
-            name: String::from(name)
-        }
-    }
-}
+// impl File {
+//     pub fn new(name: &str, size: usize) -> File {
+//         File {
+//             size: size,
+//             name: String::from(name)
+//         }
+//     }
+// }
 
-struct Directory {
-    name: String,
-    files: Vec<File>,
-    directories: Vec<Directory>,
-    parent: Directory,
-    root: Root
-}
+// struct Directory {
+//     name: String,
+//     files: Vec<File>,
+//     directories: Vec<Directory>,
+//     parent: Directory,
+//     root: Root
+// }
 
-impl Directory {
-    pub fn new(name: &str, root: Root, parent: Directory) -> Directory {
-        Directory {
-            name: String::from(name), 
-            files: vec!(), 
-            directories: vec!(),
-            parent: parent,
-            root: root
-        }
-    }
-}
+// impl Directory {
+//     pub fn new(name: &str, root: Root, parent: Directory) -> Directory {
+//         Directory {
+//             name: String::from(name), 
+//             files: vec!(), 
+//             directories: vec!(),
+//             parent: parent,
+//             root: root
+//         }
+//     }
+// }
 
-struct Root {
-    files: Vec<File>,
-    directories: Vec<Directory>,
-}
+// struct Root {
+//     files: Vec<File>,
+//     directories: Vec<Directory>,
+// }
 
